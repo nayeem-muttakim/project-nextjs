530c3c87b0f353b0b3993133ffa46e937fc9a678
Nohan550 <nohanmuttakim.bd@gmail.com>
2024-08-12 20:20:19 +0600

    card component change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export const revalidate = 0;
 export default async function Home() {
   const response = await fetch("http://localhost:5500/products");
   const products = await response.json();
-  console.log(products);
+  // console.log(products);
   return (
-    <main className="">
+    <main className="max-w-7xl mx-auto my-16">
       {products.map((product) => (
         <Card key={product.id} product={product}/>
       ))}
--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -4,13 +4,35 @@ const Card = ({ product }) => {
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <figure className="px-10 pt-10">
-        <Image
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt=""
-          width={200}
-          height={200}
-          className="rounded-xl"
-        />
+        <div className="carousel rounded-box w-96">
+          <div className="carousel-item w-1/2">
+            <Image
+              height={300}
+              width={300}
+              src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+              className="w-full"
+              alt=""
+            />
+          </div>
+          <div className="carousel-item w-1/2">
+            <Image
+              height={300}
+              width={300}
+              src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+              className="w-full"
+              alt=""
+            />
+          </div>
+          <div className="carousel-item w-1/2">
+            <Image
+              height={300}
+              width={300}
+              src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+              className="w-full"
+              alt=""
+            />
+          </div>
+        </div>
       </figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title">{product.title}</h2>
